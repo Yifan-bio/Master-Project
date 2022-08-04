bc4ce0b0b83af0b8ca0cb14d61f3b2bb5cc670f5
Ivanchaoshui <87715781+Ivanchaoshui@users.noreply.github.com>
2022-08-04 15:19:17 +0200

    Update Index.sh

--- Script/ATAC/Index.sh
+++ Script/ATAC/Index.sh
@@ -15,39 +15,30 @@ usage() {
 	echo -e "Usage: $0 [options]"
 	echo -e ""	
 	echo -e "-g <string>		Genome files used to make bowtie2 index file"	
-	echo -e "-b <string>		Blacklist file from ENCODE project (boyle-lab)"
-	echo -e "-o <string>		Output directory with all result files"
 	exit 1
 }
 
 #Initiate parameters with NULL
 GenomeFasta=""
 
-while getopts ":r1:r2:i:g:b:o:p:" op; do
+while getopts ":g:b:" op; do
 	case $op in
-		r1) R1=${OPTARG} ;;
-		r2) R2=${OPTARG} ;;
-		i) Index=${OPTARG} ;;
 		g) GenomeFasta=${OPTARG} ;;
 		b) blacklist=${OPTARG} ;;
-		o) WDIR=${OPTARG} ;;
-        p) prefix=${OPTARG} ;;
-		*) usage ;;
+        o) WDIR=${OPTARG} ;;
+        \?) usage ;;
 	esac
 done
 shift $((OPTIND-1))
 
 # check necessary parameters
-if [[ -z $R1 ]] || [[ -z $R2 ]] || [[ -z $Index ]]; then
+if [[ -z $g ]] || [[ -z $b ]]; then
 	echo -e "No index, no reads, no analysis"
 	usage
 	exit -1
 fi
 
 #Get absolute file path, so users can use relative/absolute as they like.
-[[ ${R1} != "" ]] && R1=`realpath ${R1}`
-[[ ${R2} != "" ]] && R2=`realpath ${R2}`
-[[ ${Index} != "" ]] && Index=`realpath ${Index}`
 [[ ${GenomeFasta} != "" ]] && GenomeFasta=`realpath ${GenomeFasta}`
 [[ ${blacklist} != "" ]] && blacklist=`realpath ${blacklist}`
 [[ ${WDIR} != "" ]] && WDIR=`realpath ${WDIR}`
@@ -55,3 +46,16 @@ fi
 # make directories if not exist and enter working directory.
 [[ ! -d ${WDIR} ]] && mkdir -p ${WDIR}
 cd ${WDIR}
+
+
+##########################################################################
+echo "#########################################################################"
+echo "Indexing genome"
+echo "#########################################################################"
+##########################################################################
+
+#Indexing genome
+bowtie2 index -p 8 -x ${WDIR}/genome -f ${GenomeFasta}
+
+# Getting the genome size
+genome_size=`awk '{sum+=$3-$2}END{print sum}' ${GenomeFasta}`
