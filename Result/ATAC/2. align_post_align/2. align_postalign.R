b9c6385c558a935cc60bed8a93ebdbf266f3c8c2
Ivanchaoshui <87715781+Ivanchaoshui@users.noreply.github.com>
2022-09-23 16:37:18 +0200

    atac

--- Result/ATAC/2. align_post_align/2. align_postalign.R
+++ Result/ATAC/2. align_post_align/2. align_postalign.R
@@ -1,8 +1,16 @@
 # ------------------------------ Plot2A ---------------------------------------
 # Plotting the alignment
 
-
-
+x = read.delim("../DiffBind/test.txt")
+colnames(x) = c("Name","Untreat Replicate 1","Untreat Replicate 2","Treat Replicate 1","Treat Replicate 2")
+x = x %>% pivot_longer(!Name,names_to = "Condition",values_to = "read")
+y=c("Final","Filter","Duplicate","MT","Unmapped")
+ggplot(x,aes(x=read,y=Condition,fill=forcats::fct_rev(Name))) + geom_bar(stat = "identity",position="stack") +
+  scale_x_continuous(labels = unit_format(unit = "",scale = 1e-6),expand = c(0,0),limits = c(0,1.5e8)) +
+  labs(x = "Read Pairs (Million)",y="Sample") +
+  theme_bw()
+  scale_fill_manual(values=c("red4","forestgreen","lightblue","deeppink4","blue4"))
+  
 
 
 
